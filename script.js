97d0be13033ca09fa650a724f2328fc473915b7e
Patrycja-123 <111731091+Patrycja-123@users.noreply.github.com>
2022-09-09 15:34:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function zmienna(){
+	document.getElementById("container1").style.display="none";
+	document.getElementById("container2").style.display="block";
+	if (document.querySelector( ':focus' )){
+		document.getElementById("zmien").innerHTML="dupa napis";}}
+		
